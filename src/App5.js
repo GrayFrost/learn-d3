73c2477e775298f047fb3bb5272dadd51b002cc7
zhangzhihui <zhang.zhihui@intellif.com>
2019-11-26 20:53:12 +0800

    feat(Graph-add-icon): learn add other image in a g svg container

--- src/App5.js
+++ src/App5.js
@@ -1,16 +1,18 @@
 import React from "react";
-import Graph from "./Graph";
+import Graph from "./Graph-add-icon";
 import img1 from "./asset/image2/1.jpg";
 import img2 from "./asset/image2/2.jpg";
 import img3 from "./asset/image2/3.jpg";
+import svg1 from "./asset/svg/icon-huji.svg";
+import svg2 from "./asset/svg/icon-wz.svg";
 
 class App extends React.Component {
   count = 2;
   imageArr = [img1, img2, img3];
   componentDidMount() {
     this.nodes = [
-      { id: 1, name: "小孩", src: img1 },
-      { id: 2, name: "保安", src: img3 }
+      { id: 1, name: "小孩", src: img1, icons: [{ src: svg1 }, { src: svg2 }] },
+      { id: 2, name: "保安", src: img3, icons: [{ src: svg2 }] }
     ];
     this.edges = [{ id: 1, source: 1, target: 2, count: 10 }];
     this.graph = new Graph(
